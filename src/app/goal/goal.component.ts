c1faa24beae55b5c50b28e4ef0a16801d8e702a9
Samwelrangili <samwelrangili896@gmail.com>
2020-03-26 12:40:23 +0300

    added a strikethrough effect

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -18,21 +18,21 @@ export class GoalComponent implements OnInit {
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
-// deleteGoal(isComplete, index){
-//   if(isComplete){
-//     let toDelete=confirm('Are you sure you want delete ${this.goals[index].name}?');
+deleteGoal(isComplete, index){
+  if(isComplete){
+    let toDelete=confirm('Are you sure you want delete ${this.goals[index].name}?');
 
-//     if(toDelete){
-//       this.goals.splice(index,1);
-//     }
-//   }
-// }
-// addNewGoal(goal){
-//   let goalLength = this.goals.length;
-//   goal.id = goalLength+1;
-//   goal.completeDate = new Date(goal.completeDate)
-//   this.goals.push(goal)
-// }
+    if(toDelete){
+      this.goals.splice(index,1);
+    }
+  }
+}
+addNewGoal(goal){
+  let goalLength = this.goals.length;
+  goal.id = goalLength+1;
+  goal.completeDate = new Date(goal.completeDate)
+  this.goals.push(goal)
+}
   constructor() { }
 
   ngOnInit() {
